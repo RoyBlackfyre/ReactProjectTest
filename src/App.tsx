47801a451bc5beca4b95ab65b059260a83e56ccc
Roel Urquidez <roelul@outlook.com>
2026-02-13 12:11:59 -0700

    Se añadio funcionalidad  a to do list, para ser actualizable

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,29 @@ import TodoItem from './components/TodoItem';
 
 function App() {
 
+  const[todos,setTodos] = useState(dummyData)
+
+  function setTodoCompleted(id:number, completed:boolean){
+    setTodos((prevTodos) => 
+      prevTodos.map(todo => (
+        todo.id === id ? {...todo,completed} : todo
+      ))
+  );
+  }
+
   return (
     <main className="py-10 px-20 h-screen">
-      <div className='border rounded-md flex flex-col gap-2 overflow-hidden'>
-        <h1 className="font-bold text-3xl text-center bg-blue-500 mb-3 text-white">Your To-do</h1> 
-        <div className='max-w-lg mx-auto'>
-          <div className='space-y-2 mb-3'>
-            {dummyData.map((elementosTodo) =>(
-              <TodoItem key={elementosTodo.id} tareaObjeto={elementosTodo} />
+      <div className='border mx-auto rounded-md flex flex-col gap-2 overflow-hidden'>
+        <h1 className="font-bold text-3xl text-center bg-blue-500 text-white">Your To-do</h1> 
+          <div className='space-y-2 m-5 mr-4 ml-4'>
+            {todos.map((elementosTodo) =>(
+              <TodoItem 
+              key={elementosTodo.id} 
+              tareaObjeto={elementosTodo}
+              onCompletedChange={setTodoCompleted}
+              />
             ))}
-          </div>
+          
         </div>
       </div>
     </main>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,21 +2,21 @@ import type { Todo } from "../types/todo"
 
 interface TodoItemProps {
     tareaObjeto: Todo;
+    onCompletedChange: (id:number,completed:boolean) => void;
 }
 
-export default function TodoItem({tareaObjeto}: TodoItemProps) {
+export default function TodoItem({tareaObjeto,onCompletedChange}: TodoItemProps) {
     return(
         
             <label className="flex gap-4 item-center border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
                 <input 
                 type="checkbox"
+                checked={tareaObjeto.completed}
+                onChange={(e)=> onCompletedChange(tareaObjeto.id,e.target.checked)}
                 className="scale-125"
                 />
                 <span className={tareaObjeto.completed?"line-through":""}>
                     {tareaObjeto.title}
-                </span>
-                <span>
-                    {tareaObjeto.completed? " ✅" : " ❌"}
                 </span>
             </label>
             
